e9c13d63eb344ab75ca338e23fea708fa130c021
farzaliev_g <grishka2500@gmail.com>
2025-02-26 17:34:25 +0400

    fix

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,19 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from "./App.vue";
 import { router } from "./providers/index";
 
 import "@/shared/ui/style/app.scss";
+import { apolloClient } from '@/shared/api/apolloClientInstance';
 
-const app = createApp(App);
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(router);
 
--- src/pages/home/ui/Home.vue
+++ src/pages/home/ui/Home.vue
@@ -1,20 +1,65 @@
 <template>
   <div class="home-page">
+    {{ resultComputed }}
+
+    <div @click="wefewf">wefwfee</div>
     <BitcoinRateComponent />
     <VisualInvestmentPortfolio />
   </div>
 </template>
 
-
 <script lang="ts" setup>
+import { apolloClient } from "@/shared/api/apolloClientInstance";
 import { BitcoinRateComponent } from "@/widgets/bitcoin-rate-component";
 import { VisualInvestmentPortfolio } from "@/widgets/visual-investment-portfolio";
-</script>
+import {
+  provideApolloClient,
+  useLazyQuery,
+  useQuery,
+} from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import { computed } from "vue";
 
+const { result, loading, error, refetch, load } = useLazyQuery(gql`
+  query ExampleQuery {
+    capsulesPast {
+      dragon {
+        active
+        crew_capacity
+        description
+        diameter {
+          feet
+          meters
+        }
+        first_flight
+        wikipedia
+      }
+      id
+      landings
+      missions {
+        flight
+        name
+      }
+      status
+      reuse_count
+      original_launch
+    }
+  }
+`);
+
+console.log(result.value);
+
+const resultComputed = computed(() => result.value);
+
+const wefewf = async () => {
+  try {
+    let data = await load();
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
 
 <style lang="scss">
 @import "./style.scss";
 </style>
-
-
-
